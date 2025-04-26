f2d78e2889f321b3787390a98f97c46c19602858
k-kanke <kanke.kooooo01@gmail.com>
2025-04-26 19:35:06 +0900

    カテゴリ別検索の実装

--- recomeshi/src/components/SearchForm.jsx
+++ recomeshi/src/components/SearchForm.jsx
@@ -1,5 +1,12 @@
 import React, {useState} from 'react';
 
+const items = {
+    野菜: ["大根", "玉ねぎ", "人参", "白菜", "キャベツ", "ネギ", "じゃがいも"],
+    肉: ["鶏もも肉", "豚バラ", "豚ロース", "牛肉", "ひき肉"],
+    魚: ["鮭", "鯖", "アジ", "タラ", "いわし"]
+}
+
+
 const SearchForm = () => {
     const [keyword, setKeyword] = useState('');
 
@@ -9,10 +16,34 @@ const SearchForm = () => {
             const url = `https://cookpad.com/jp/search/${encodedKeyword}`;
             window.open(url, '_blank')
         }
-    }
+    };
+
+    // カテゴリボタンを押した時
+    const handleCategoryClick = (category) => {
+        const categoryItems = items[category];
+        if (categoryItems && categoryItems.length > 0) {
+        const randomItem = categoryItems[Math.floor(Math.random() * categoryItems.length)];
+        setKeyword(randomItem);
+        }
+    };
 
     return (
         <div style={{ marginTop: '1rem' }}>
+            <h2>カテゴリ別提案</h2>
+            {/* カテゴリボタン */}
+            <div style={{ marginBottom: '1rem' }}>
+                {Object.keys(items).map((category) => (
+                    <button
+                        key={category}
+                        onClick={() => handleCategoryClick(category)}
+                        style={{ margin: '0.5rem', padding:'0.5rem 1rem' }}
+                    >
+                        {category}
+                    </button>
+                ))}
+            </div>
+
+            {/* 検索ボックスとボタン */}
             <input
                 type="text" 
                 placeholder='食材を入力...'
